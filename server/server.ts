ba6008a7636c27f7a3726877d4174126a8b03563
zephyr <angelanznz@hotmail.com>
2023-04-13 16:20:01 +1200

    add users and cards routers

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,9 @@ import express from 'express'
 import path from 'path'
 import { join } from 'node:path'
 
+import users from './routes/users'
 import categories from './routes/categories'
+import cards from './routes/cards'
 import authRoutes from './routes/auth'
 
 const server = express()
@@ -10,7 +12,9 @@ const server = express()
 server.use(express.json())
 server.use(express.static(join(__dirname, 'public')))
 
+server.use('/api/v1/users', users)
 server.use('/api/v1/categories', categories)
+server.use('/api/v1/cards', cards)
 server.use('api/v1/auth', authRoutes)
 server.get('*', (req, res) => {
   const appPath = path.join(__dirname, 'public', 'index.html')
