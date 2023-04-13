eb927af3ed47d5dc39b3715ac4a361f0a7685bee
박지호 <yoho4613@gmail.com>
2023-04-13 15:02:34 +1200

    init

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -4,24 +4,13 @@ import { JwtRequest } from '../auth0'
 import checkJwt from '../auth0'
 import { requiresAuth } from 'express-openid-connect'
 
-// import {
-//   getFruits,
-//   addFruit,
-//   deleteFruit,
-//   updateFruit,
-//   userCanEdit,
-// } from '../db/fruits'
 
 const router = express.Router()
 
 
-// router.get('/profile', requiresAuth(), (req, res) => {
-//   res.send(JSON.stringify(req.oidc.user));
-// });
 
 
-// A public endpoint that anyone can access
-// GET /api/v1/fruits
+
 router.get('/', (req, res) => {
  
 
@@ -30,9 +19,7 @@ router.get('/', (req, res) => {
 
 
 router.post('/', checkJwt, (req: JwtRequest, res) => {
-  const { fruit } = req.body
   const auth0Id = req.auth?.sub
-
   if (!auth0Id) {
     console.error('No auth0Id')
     return res.status(401).send('Unauthorized')
@@ -44,14 +31,7 @@ router.post('/', checkJwt, (req: JwtRequest, res) => {
 
 
 router.put('/', checkJwt, (req: JwtRequest, res) => {
-  const { fruit } = req.body
   const auth0Id = req.auth?.sub
-  const fruitToUpdate = {
-    id: fruit.id,
-    added_by_user: auth0Id,
-    name: fruit.name,
-    average_grams_each: fruit.averageGramsEach,
-  }
 
   if (!auth0Id) {
     console.error('No auth0Id')
@@ -63,9 +43,7 @@ router.put('/', checkJwt, (req: JwtRequest, res) => {
 
 
 router.delete('/:id', (req: JwtRequest, res) => {
-  const id = Number(req.params.id)
   const auth0Id = req.auth?.sub
-
   if (!auth0Id) {
     console.error('No auth0Id')
     return res.status(401).send('Unauthorized')
