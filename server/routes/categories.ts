da48ab53c7f62157036e729b0e092481dc045a14
maycon <may.con.fa@hotmail.com>
2023-04-13 16:05:24 +1200

    test addData function in insomnia

--- server/routes/categories.ts
+++ server/routes/categories.ts
@@ -6,7 +6,7 @@ const router = express.Router()
 
 router.get('/', async (req, res) => {
   try {
-    const data = await db.getData('categories')
+    const data = await db.getData('users')
     res.json(data)
   } catch (error) {
     res.status(500).send(console.error(error))
@@ -17,6 +17,16 @@ router.delete('/', async (req, res) => {
   const id = req.body.id
   try {
     const data = await db.deleteData('categories', id)
+    res.json(data)
+  } catch (error) {
+    res.status(500).send(console.error(error))
+  }
+})
+
+router.post('/', async (req, res) => {
+  const newData = req.body
+  try {
+    const data = await db.addData('users', newData)
     res.json(data)
   } catch (error) {
     res.status(500).send(console.error(error))
