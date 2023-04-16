bf2232c46cf0cc2fed0a5a00d6cb054d8efbdc2f
zephyr <angelanznz@hotmail.com>
2023-04-17 10:51:36 +1200

    add horizontal stats chart

---
+++ client/components/UserStats.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import CompletedTasksBarChart from './Chart'
+
+const UserStats = () => {
+  return <CompletedTasksBarChart completedTasks={10} totalTasks={20} />
+}
+
+export default UserStats
