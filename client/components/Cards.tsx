4f1f2303182f14eed4a2bfb344c7482c865b9ff6
zephyr <angelanznz@hotmail.com>
2023-04-16 18:50:08 +1200

    fix typo

--- client/components/Cards.tsx
+++ client/components/Cards.tsx
@@ -74,13 +74,7 @@ const Cards = (props: Props) => {
             className={
               'cursor-pointer opacity-90 dark:active:shadow-[0_8px_9px_-4px_rgba(51, 51, 51, 0.2),0_4px_18px_0_rgba(51, 51, 51,0.1)]] inline-block rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#8c8c8c] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(51, 51, 51,0.3),0_4px_18px_0_rgba(51, 51, 51, 0.2)]  focus:shadow-[0_8px_9px_-4px_rgba(51, 51, 51,0.3),0_4px_18px_0_rgba(51, 51, 51, 0.2)] focus:outline-none focus:ring-0active:shadow-[0_8px_9px_-4px_rgba(51, 51, 51,0.3),0_4px_18px_0_rgba(51, 51, 51, 0.2)] dark:shadow-[0_4px_9px_-4px_rgba(51, 51, 51,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(51, 51, 51, 0.2),0_4px_18px_0_rgba(51, 51, 51,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(51, 51, 51, 0.2),0_4px_18px_0_rgba(51, 51, 51,0.1)]'
             }
-            style={{
-              background: card.completed ? '#333333' : '#48BB78',
-              transition: 'background-color 0.25s ease-in-out',
-              ':hover': {
-                background: card.completed ? '#282828' : '#3CBB85',
-              },
-            }}
+            style={{ background: card.completed ? '#333333' : '#48BB78' }}
             htmlFor={String(card.id)}
           >
             {card.completed ? 'Unmark' : 'Mark as done'}
