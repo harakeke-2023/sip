2dfdbe720e9dd64e5096490760c88bc415ef2ed1
Dionatan Narciso <dionatan.narciso@gmail.com>
2023-04-18 14:47:18 +1200

    unbroken styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,21 +12,6 @@ module.exports = {
       'animate-[tada_1s_ease-in-out]',
       'animate-[tada_1s_ease-in-out]',
     ],
-    colors: {
-      tahiti: {
-        50: '#f0f4fd',
-
-        100: '#cffafe',
-        200: '#a5f3fc',
-        300: '#67e8f9',
-        400: '#22d3ee',
-        500: '#06b6d4',
-        600: '#0891b2',
-        700: '#0e7490',
-        800: '#155e75',
-        900: '#164e63',
-      },
-    },
     // colors: {
     //     50: '#f0f4fd',
     //     100: '#e4ebfb',
