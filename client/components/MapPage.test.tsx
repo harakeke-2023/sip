2b4108961a4c5db84f33a6e6c86f328912a078a9
박지호 <yoho4613@gmail.com>
2023-04-19 14:34:45 +1200

    test with 3files

--- client/components/MapPage.test.tsx
+++ client/components/MapPage.test.tsx
@@ -4,7 +4,6 @@ import MapPage from './MapPage'
 import { StateContext } from '../context/StateContext'
 import '@testing-library/jest-dom/extend-expect'
 
-
 describe('MapPage', () => {
   it('renders without crashing', () => {
     render(
@@ -13,24 +12,6 @@ describe('MapPage', () => {
       </StateContext>
     )
   })
-
-  it('displays map with markers when there are cards', async () => {
-    const { container } = render(
-      <StateContext>
-        <MapPage />
-      </StateContext>
-    )
-
-    await waitFor(() => {
-      const map = screen.getByRole('presentation');
-      expect(map).toBeInTheDocument();
-    }, { timeout: 5000 });
-
-    // Check that there are markers on the map
-    const markers = screen.getAllByRole('img');
-    expect(markers).toHaveLength(2);
-  });
-  
 
   it('does not display markers when there are no cards', () => {
     const { container } = render(
