ab8e6cf65592338fd87f7a8013d5e5b6751f20db
zephyr <angelanznz@hotmail.com>
2023-04-19 14:24:39 +1200

    comment out some extra code

--- client/components/Cards.test.tsx
+++ client/components/Cards.test.tsx
@@ -22,22 +22,22 @@ test('renders the update button', () => {
   expect(buttons).toHaveLength(1)
 })
 
-test('renders the update button', () => {
-  // Arrange
-  const props = {
-    categoryId: 1,
-    userId: 2,
-    fetchCards: jest.fn(),
-  }
+// test('renders the update button', () => {
+//   // Arrange
+//   const props = {
+//     categoryId: 1,
+//     userId: 2,
+//     fetchCards: jest.fn(),
+//   }
 
-  // Act
-  const { container } = render(
-    <StateContext>
-      <Cards {...props} />
-    </StateContext>
-  )
+//   // Act
+//   const { container } = render(
+//     <StateContext>
+//       <Cards {...props} />
+//     </StateContext>
+//   )
 
-  // Assert
-  const buttons = container.querySelectorAll('button')
-  expect(buttons).toHaveLength(1)
-})
+//   // Assert
+//   const buttons = container.querySelectorAll('button')
+//   expect(buttons).toHaveLength(1)
+// })
